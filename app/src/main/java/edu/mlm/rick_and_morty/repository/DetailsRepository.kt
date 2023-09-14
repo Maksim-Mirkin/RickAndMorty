ea93d1b10d4e72a11e8dc98286ff4fde720be505
Maksim-Mirkin <maksim.mirkin@gmail.com>
2023-09-14 18:56:30 +0300

    fixed long api request in details repo

--- app/src/main/java/edu/mlm/rick_and_morty/repository/DetailsRepository.kt
+++ app/src/main/java/edu/mlm/rick_and_morty/repository/DetailsRepository.kt
@@ -3,8 +3,12 @@ package edu.mlm.rick_and_morty.repository
 import edu.mlm.rick_and_morty.service.RAMService
 import edu.mlm.rick_and_morty.service.dto.character.CharacterAPI
 import edu.mlm.rick_and_morty.service.dto.episode.EpisodeAPI
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DetailsRepository @Inject constructor(private val service: RAMService) {
@@ -34,12 +38,17 @@ class DetailsRepository @Inject constructor(private val service: RAMService) {
      * to get the episode by ID, and adds it to the final returned list.
      */
     suspend fun getEpisodeList(episodes: List<String>): List<EpisodeAPI> {
-        val episodeItems: MutableList<EpisodeAPI> = mutableListOf()
-        episodes.map { episodeUrl ->
-            val episodeId = episodeUrl.substringAfterLast('/').toInt()
-            episodeItems.add(service.getEpisode(episodeId))
+        val episodeItems = mutableListOf<Deferred<EpisodeAPI>>()
+        withContext(Dispatchers.IO) {
+            episodes.map { episodeUrl ->
+                val episodeId = episodeUrl.substringAfterLast('/').toInt()
+                val episode = async {
+                    service.getEpisode(episodeId)
+                }
+                episodeItems.add(episode)
+            }
         }
-        return episodeItems
+        return episodeItems.awaitAll()
     }
 
     /**
@@ -47,12 +56,17 @@ class DetailsRepository @Inject constructor(private val service: RAMService) {
      * to get the character by ID, and adds it to the final returned list.
      */
     suspend fun getCharacterList(characters: List<String>): List<CharacterAPI> {
-        val characterItems: MutableList<CharacterAPI> = mutableListOf()
-        characters.map { item ->
-            val characterId = item.substringAfterLast('/').toInt()
-            characterItems.add(service.getCharacter(characterId))
+        val characterItems = mutableListOf<Deferred<CharacterAPI>>()
+        withContext(Dispatchers.IO){
+            characters.map { characterUrl ->
+                val characterId = characterUrl.substringAfterLast('/').toInt()
+                val character = async{
+                    service.getCharacter(characterId)
+                }
+                characterItems.add(character)
+            }
         }
-        return characterItems
+        return characterItems.awaitAll()
     }
 
     /**
